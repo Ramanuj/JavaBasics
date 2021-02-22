f4e6dc6e4919385e22e1d31030c023216d32e16b
Ramanuj <ramanuj.srivastava21@gmail.com>
2021-02-22 15:36:03 -0500

    changes for lambda comparison

--- src/main/java/com/ramanuj/javabasic/collections/ObjectComparisonsLambda.java
+++ src/main/java/com/ramanuj/javabasic/collections/ObjectComparisonsLambda.java
@@ -1,5 +1,7 @@
 package com.ramanuj.javabasic.collections;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import com.ramanuj.javabasic.data.Product;
@@ -19,7 +21,32 @@ public class ObjectComparisonsLambda {
 			PopulateProduct populateProduct = new PopulateProduct();
 			List<Product> productList = populateProduct.getProducts();
 
-			PrintHelper.PrintProductList(productList);
+			PrintHelper.printProductList(productList);
+
+			System.out.println("Sorting based on name");
+			/**
+			 * No need of specifying the type definition. Compiler infers it by default.
+			 */
+			productList.sort((p1, p2) -> p1.getName().compareTo(p2.getName()));
+			PrintHelper.printProductList(productList);
+
+			/**
+			 * 
+			 */
+			System.out.println("Sorting by name and then rating");
+			productList.sort(Product::compareByNameThenRating);
+			PrintHelper.printProductList(productList);
+
+			System.out.println("Sorting by brand and then name");
+			productList.sort(Product::compareByBrandThenName);
+			PrintHelper.printProductList(productList);
+
+			Collections.sort(productList, Comparator.comparing(Product::getPrice));
+			PrintHelper.printProductList(productList);
+
+			System.out.println("Sorting by brand and then name again");
+			productList.sort(Comparator.comparing(Product::getBrand).thenComparing(Product::getName));
+			PrintHelper.printProductList(productList);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
